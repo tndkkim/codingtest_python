8a52949fb7d49c6ddae4dd94016a1473d33b6c1c
tndkkim <100478158+tndkkim@users.noreply.github.com>
2024-12-18 12:07:14 +0900

    [Silver III] Title: 소수 구하기, Time: 280 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,19 @@
 M, N = map(int, input().split())
 
-for num in range(M, N+1):
-    if num < 2:
-        continue
-    is_prime = True
-    for i in range(2, num):
-        if num % i == 0:
-            is_prime = False
-            break
-        if i * i > num:
-            break
+# 초기화: N+1 크기의 리스트를 True로 채움
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False
 
-    if is_prime:
-        print(num)
+# 에라토스테네스의 체 적용
+i = 2
+while i * i <= N:
+    if is_prime[i]:
+        # i의 배수들을 모두 False로 변경
+        for j in range(i * i, N + 1, i):
+            is_prime[j] = False
+    i += 1
+
+# M부터 N까지의 소수 출력
+for i in range(M, N + 1):
+    if is_prime[i]:
+        print(i)
