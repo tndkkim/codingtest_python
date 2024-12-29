a785590f8a925102ab6c3472d33eab002e830213
tndkkim <100478158+tndkkim@users.noreply.github.com>
2024-12-29 18:26:20 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,11 @@
+t = int(input())
+dp = [0] * 101
+dp[1] = dp[2] = dp[3] = 1
+dp[4] = dp[5] = 2
+
+for i in range(6, 101):
+    dp[i] = dp[i-1] + dp[i-5]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
